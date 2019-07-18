0232c691c15c65879730136969c011d0019f2738
zhy <794182811@qq.com>
2019-07-18 14:28:12 +0800

    roma转整数

--- src/exercise/demo/inttoroman/IntToRoman.java
+++ src/exercise/demo/inttoroman/IntToRoman.java
@@ -9,8 +9,6 @@ public class IntToRoman {
 	public String intToRoman(int num) {
 		int temp = 1000;
 		int index = 0;
-		HashMap<Integer, String> changeValue = new 
-		List<String> strList = new ArrayList<String>(Arrays.asList("I","V","X","L","C","D","M"));
 		while(temp != 0) {
 			
 		}
---
+++ src/exercise/demo/romatoint/Demo.java
@@ -0,0 +1,8 @@
+package exercise.demo.romatoint;
+
+public class Demo {
+public static void main(String[] args) {
+	RomaToInt romaToInt = new RomaToInt();
+	System.out.println(romaToInt.romanToInt("VVVIVIV"));
+}
+}
