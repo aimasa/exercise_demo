e940a99e00e363e2b4a6ac5884a859392055df41
aimasa <794182811@qq.com>
2019-08-23 22:13:56 +0800

    暂时提交

--- src/exercise/demo/romatoint/Demo.java
+++ src/exercise/demo/romatoint/Demo.java
@@ -2,7 +2,15 @@ package exercise.demo.romatoint;
 
 public class Demo {
 public static void main(String[] args) {
-	RomaToInt romaToInt = new RomaToInt();
-	System.out.println(romaToInt.romanToInt("VVVIVIV"));
+//	RomaToInt romaToInt = new RomaToInt();
+//	System.out.println(romaToInt.romanToInt("VVVIVIV"));
+	
+    byte[] haha2 = "hahaha".getBytes();
+    System.out.println("toString()转换byte[]: "+haha2.toString());
+
+    byte[] haha = "hahaha".getBytes();
+	
+	System.out.println("new String()转换byte[]: "+new String(haha));
+
 }
 }
