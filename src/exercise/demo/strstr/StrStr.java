02e804c98ba020600ee24ca070eb765b0e3d52ef
曾寒毓 <794182811@qq.com>
2019-12-11 13:33:16 +0800

    strstr()还没写完，暂存

--- src/exercise/demo/strstr/StrStr.java
+++ src/exercise/demo/strstr/StrStr.java
@@ -2,8 +2,15 @@ package exercise.demo.strstr;
 
 public class StrStr {
     public int strStr(String haystack, String needle) {
-        
+        char[] haystackChar = haystack.toCharArray();
+        char[] needleChar = needle.toCharArray();
+        int[] next = getNext(needle);
         return 0;
+        
+    }
+    public int[] getNext(String needle) {
+       // 因为前面需要比较的子字符串已经比较过了是相等的，所以下一个跳转位置就能够从匹配字符串中得到。懂吧懂吧，晚上回来写完 
+        return null;
         
     }
 }
