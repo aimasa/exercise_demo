6759e3483de9ba7b1a0713cd76f9f175d9c26062
aimasa <794182811@qq.com>
2020-01-28 21:06:58 +0800

    1、temp commit

---
+++ src/exercise/demo/removenthfromend/RemoveNthFromEnd.java
@@ -0,0 +1,13 @@
+package exercise.demo.removenthfromend;
+
+public class RemoveNthFromEnd {
+	 public class ListNode {
+		      int val;
+		      ListNode next;
+		      ListNode(int x) { val = x; }
+		  }
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+		return head;
+        
+    }
+}
